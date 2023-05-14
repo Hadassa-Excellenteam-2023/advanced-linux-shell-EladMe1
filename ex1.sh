02b08af6b90876109bfc12d08d9eb419d5c686dc
Elad Menahem <93273498+EladMe1@users.noreply.github.com>
2023-05-15 00:13:06 +0300

    finish ex1

---
+++ ex1.sh
@@ -0,0 +1,16 @@
+echo "Elad"> my_name.txt
+cat>my_name.txt << EOF
+Elad
+EOF
+mkdir ~/new
+cp my_name.txt ~/new
+mv ~/new/my_name.txt ~/new/Elad.txt
+ ~/new/Elad.txt
+gedit ~/new/Elad.txt
+cp ~/new/Elad.txt ~/new/newFile.txt
+cd ~/new
+ls
+cd $HOME
+~ cd
+rm -rf ~/new
+ 
